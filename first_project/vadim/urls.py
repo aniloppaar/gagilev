5bee0dc3fba1000ab54b2f7773bb9fba61382366
Your Name <you@example.com>
2025-11-29 16:41:27 +0500

    detail

--- first_project/vadim/urls.py
+++ first_project/vadim/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('tours/', views.tour_list, name='tour_list'),
-    path('tours/<int:tour_id>/', views.tour_detail, name='tour_detail'),
-    path('hotels/', views.hotel_list, name='hotel_list'),
-    path('hotels/<int:hotel_id>/', views.hotel_detail, name='hotel_detail'),
+    path('tours/', views.tours, name='tours'),
+    path('tour/<int:tour_id>/', views.tour_detail, name='tour_detail'),
+    path('hotels/', views.hotels, name='hotels'),
+    path('hotel/<int:hotel_id>/', views.hotel_detail, name='hotel_detail'),
 ]
--- first_project/vadim/views.py
+++ first_project/vadim/views.py
@@ -3,47 +3,45 @@ from django.db.models import Q
 from .models import Tour, Hotel, Reviews
 
 def home(request):
-    # Показываем только популярные туры и отели на главной
     tours = Tour.objects.select_related('hotel', 'flight', 'hotel__room_class')[:6]
     hotels = Hotel.objects.select_related('room_class')[:6]
-    reviews = Reviews.objects.select_related('client', 'tour')[:8]
     
     context = {
         'tours': tours,
         'hotels': hotels,
-        'reviews': reviews,
     }
     return render(request, 'home.html', context)
 
-def tour_list(request):
-    # Поиск туров
+def tours(request):
     search_query = request.GET.get('q', '')
+    price_min = request.GET.get('price_min', '')
+    price_max = request.GET.get('price_max', '')
+    
+    tours = Tour.objects.select_related('hotel', 'flight', 'hotel__room_class')
     
     if search_query:
-        tours = Tour.objects.filter(
+        tours = tours.filter(
             Q(name__icontains=search_query) |
             Q(hotel__name__icontains=search_query) |
             Q(hotel__place__icontains=search_query)
-        ).select_related('hotel', 'flight', 'hotel__room_class')
-    else:
-        tours = Tour.objects.select_related('hotel', 'flight', 'hotel__room_class').all()
+        )
+    
+    if price_min:
+        tours = tours.filter(price__gte=price_min)
+    
+    if price_max:
+        tours = tours.filter(price__lte=price_max)
     
     context = {
         'tours': tours,
         'search_query': search_query,
-        'is_search': bool(search_query),
+        'price_min': price_min,
+        'price_max': price_max,
+        'is_search': bool(search_query or price_min or price_max),
     }
     return render(request, 'tours.html', context)
 
-def tour_detail(request, tour_id):
-    tour = get_object_or_404(
-        Tour.objects.select_related('hotel', 'flight', 'hotel__room_class'),
-        id=tour_id
-    )
-    return render(request, 'tours.html', {'tour': tour})
-
-def hotel_list(request):
-    # Поиск отелей
+def hotels(request):
     search_query = request.GET.get('q', '')
     
     if search_query:
@@ -61,9 +59,28 @@ def hotel_list(request):
     }
     return render(request, 'hotels.html', context)
 
+def tour_detail(request, tour_id):
+    tour = get_object_or_404(
+        Tour.objects.select_related('hotel', 'flight', 'hotel__room_class'),
+        id=tour_id
+    )
+    reviews = Reviews.objects.filter(tour=tour).select_related('client')
+    
+    context = {
+        'tour': tour,
+        'reviews': reviews,
+    }
+    return render(request, 'tour_detail.html', context)
+
 def hotel_detail(request, hotel_id):
     hotel = get_object_or_404(
         Hotel.objects.select_related('room_class'),
         id=hotel_id
     )
-    return render(request, 'hotels.html', {'hotel': hotel})
+    tours = Tour.objects.filter(hotel=hotel)
+    
+    context = {
+        'hotel': hotel,
+        'tours': tours,
+    }
+    return render(request, 'hotel_detail.html', context)
